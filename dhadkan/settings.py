e98dcc852fc097d52bed4e3c4ea859901725102d
hulksmasher <somesh.08.96@gmail.com>
2018-01-16 23:39:56 +0530

    Lab config

--- dhadkan/settings.py
+++ dhadkan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wl-h^c)a!#n6f+&n!hjyq))a#mk8(&rf)#g_485y34yd%fuu_@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['88b72ec6.ngrok.io', 'localhost', '127.0.0.1', '192.168.103.187']
+ALLOWED_HOSTS = ['88b72ec6.ngrok.io', 'localhost', '127.0.0.1', '192.168.103.187', 'compbio.iitr.ac.in']
 
 
 # Application definition
